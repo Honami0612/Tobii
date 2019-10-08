7f2e8bf3462e538eb76352fc3fdc7d71cabff509
Honami0612 <ciho16007@g.nihon-u.ac.jp>
2019-10-08 16:53:59 +0200

    instance fix

--- Assets/Prefab/Gazeeeeeeee.cs
+++ Assets/Prefab/Gazeeeeeeee.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 
 public class Gazeeeeeeee : MonoBehaviour {
     private GazeAware gazeAware;
+    private eyePosition eyePosition;
+
 
     //private int point = 100;
 
 	// Use this for initialization
 	void Start () {
         gazeAware = GetComponent<GazeAware>();
-        
-	}
+        eyePosition= GameObject.Find("MainCamera").GetComponent<eyePosition>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,24 +26,25 @@ public class Gazeeeeeeee : MonoBehaviour {
         if (flag)
         {
             Destroy(this.gameObject);
-            /*if (gameObject.tag == "Fruits")
+            if (gameObject.tag == "Fruits")
             {
+                eyePosition.AddPoint();
                 Destroy(this.gameObject);
-                point += 30;
             }
             else if (gameObject.tag == "Poison")
             {
+                eyePosition.SubPoint();
                 Destroy(this.gameObject);
-                point -= 50;
-            }*/
+
+            }
         }
 	}
 
-  /*  public int Sentpoint()
-    {
-        get{ return point; }
-        set{ point = value; }
-    }
-    */
+    //public int Point
+    //{
+    //    get{ return point; }
+    //    set{ point = value; }
+    //}
+
 
 }
